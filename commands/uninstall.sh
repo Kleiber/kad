1ab152319dad02360909ce14ad1e0c44cfc932b8
Kleiber Ttito <kleiber100892@gmail.com>
2021-07-16 01:22:56 -0300

    improve uninstall options

--- commands/uninstall.sh
+++ commands/uninstall.sh
@@ -24,6 +24,36 @@ Run 'kad COMMAND --help' for more information about a given command.
 EOF
 }
 
+uninstall_golang() {
+    if [ -f /usr/local/bin/helm ]; then
+        echo "Uninstalling golang..."
+        sudo rm -rf /usr/local/go
+        echo "Successfully uninstalled golang."
+    else
+        echo "Nothing to uninstall."
+    fi
+}
+
+uninstall_helm() {
+    if [ -f /usr/local/bin/helm ]; then
+        echo "Uninstalling helm..."
+        sudo rm /usr/local/bin/helm
+        echo "Successfully uninstalled helm."
+    else
+        echo "Nothing to uninstall."
+    fi
+}
+
+uninstall_kubectl() {
+    if [ -f /usr/local/bin/kubectl ]; then
+        echo "Uninstalling kubectl..."
+        sudo rm /usr/local/bin/kubectl
+        echo "Successfully uninstalled kubectl."
+    else
+        echo "Nothing to uninstall."
+    fi
+}
+
 uninstall_k3s() {
     if [ -f /usr/local/bin/k3s-uninstall.sh ]; then
         echo "Uninstalling k3s..."
@@ -38,6 +68,15 @@ uninstall_cmd() {
     local os_type=$(get_os_type)
 
     case ${1} in
+        golang)
+            uninstall_golang
+            ;;
+        helm)
+            uninstall_helm
+            ;;
+        kubectl)
+            uninstall_kubectl
+            ;;
         k3s)
             uninstall_k3s
             ;;
