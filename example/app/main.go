f996c0dbf7be9bc5c9addc799610a249e6f4da6f
Kleiber Ttito <kleiber100892@gmail.com>
2021-07-24 15:36:32 -0300

    update example

--- example/app/main.go
+++ example/app/main.go
@@ -2,8 +2,40 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/gorilla/mux"
 )
 
+const (
+	host = "localhost"
+	port = "8080"
+)
+
+var (
+	appName    = os.Getenv("APP_NAME")
+	appVersion = os.Getenv("APP_VERSION")
+)
+
+func GetInfo(w http.ResponseWriter, r *http.Request) {
+	info := fmt.Sprintf("Hello %s application version %s \n", appName, appVersion)
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprintf(w, info)
+}
+
 func main() {
-	fmt.Println("Starting to run Kad App...")
+	fmt.Println("Initializing the router...")
+	router := mux.NewRouter()
+	router.HandleFunc("/hello", GetInfo).Methods("GET")
+
+	fmt.Printf("Starting server %s on port %s..\n", host, port)
+
+	hostname := fmt.Sprintf("%s:%s", host, port)
+	err := http.ListenAndServe(hostname, router)
+	if err != nil {
+		message := fmt.Sprintf("Failing to start the %q server: %s", hostname, err)
+		log.Fatal(message)
+	}
 }
